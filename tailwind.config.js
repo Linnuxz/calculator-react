1708e55c3052d1477b76ee8bdb15c4b0c37614c5
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-05-18 17:36:08 +0400

    add inset utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
         extend: {
             boxShadow: {
                 'firstTheme-inset': 'inset 0px -4px 0px 0px #B3A497',
-                'firstTheme-delete-inset': 'inset 0px -4px 0px 0px #414E73',
+                'firstTheme-deleteresetinset': 'inset 0px -4px 0px 0px #414E73',
+                'firstTheme-equalinset': '0px -4px 0px 0px #93261A inset',
+                'secondTheme-inset': '0px -4px 0px 0px #A79E91 inset',
+                'secondTheme-deleteresetinset':
+                    '0px -4px 0px 0px #1B6066 inset',
+                'secondTheme-equalinset': '0px -4px 0px 0px #873901 inset',
+                'thirdTheme-inset': '0px -4px 0px 0px #881C9E inset',
+                'thirdTheme-deleteresetinset': '0px -4px 0px 0px #BE15F4 inset',
+                'thirdTheme-equalinset': '0px -4px 0px 0px #6CF9F1 inset',
             },
         },
     },
